80d58d9250ea6910198a77fc2bd1fec7c64b13dc
MythEclipse <90584806+Asepharyana71@users.noreply.github.com>
2024-07-20 22:59:39 +0700

    Refactor code and fix formatting issues

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,7 +1,7 @@
-import { Router,Request,Response } from "express";
+import { Router, Request, Response } from 'express'
 
-export const HealthRouter : Router = Router()
+export const HealthRouter: Router = Router()
 
 HealthRouter.get('/', (req: Request, res: Response) => {
-    res.status(200).send({ data: 'Hello World' })
-  })
+  res.status(200).send({ data: 'Hello World' })
+})
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,9 @@ import { HealthRouter } from './health'
 
 const _routes: Array<[string, Router]> = [['/health', HealthRouter]]
 
-export const routes =(app : Application) => {
-    _routes.forEach((route) => {
-        const [url, router] = route
-        app.use(url, router)
-    })
+export const routes = (app: Application) => {
+  _routes.forEach((route) => {
+    const [url, router] = route
+    app.use(url, router)
+  })
 }
