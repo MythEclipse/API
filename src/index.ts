49ebdf46707154e10cad8896ac518212fd8c8c48
MythEclipse <90584806+Asepharyana71@users.noreply.github.com>
2024-07-20 22:58:53 +0700

    Refactor code and add routes to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import express, { Request, Response } from 'express'
+import { routes } from './routes'
 
 const app = express()
 const port: number = 3000
 
-app.use('/', (req: Request, res: Response) => {
-  res.status(200).send({ data: 'Hello World a' })
-})
+routes(app)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
