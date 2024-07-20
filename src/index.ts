bbce0cc03cde7e5c113aab3bb0415d0a2f726200
MythEclipse <90584806+Asepharyana71@users.noreply.github.com>
2024-07-20 13:14:32 +0700

    Refactor variable names for clarity and consistency

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response } from 'express'
 
-const app = express();
-const port: number = 3000;
+const app = express()
+const port: number = 3000
 
-app.use('/', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ data: 'Hello World a' });
-});
+app.use('/', (req: Request, res: Response) => {
+  res.status(200).send({ data: 'Hello World a' })
+})
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  console.log(`Server is running on port ${port}`)
+})
